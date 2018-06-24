2cac1f905682cde65d312c1f90cd9cc614805c39
David Amores <davidamores3@gmail.com>
2018-06-24 15:43:54 +1000

    Added user location

--- map.js
+++ map.js
@@ -6,8 +6,13 @@ var MAP = {
 
 var mymap = L.map('mapid', {zoomControl: true, dragging: false}).setView([-37.796772, 144.963654], 15);
 
+//L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+//    maxZoom: 18
+//}).addTo(mymap);
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFtb3JlcyIsImEiOiJjamlwbDhmbjIweTNuM2tvNzhmOHMyMHc4In0.5J2Vqh0EG8gAVjEWGxnNBw', {
-    attribution: '',
+    attribution: 'David &hearts;\'s  &copy;',
     maxZoom: 20,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZGFtb3JlcyIsImEiOiJjamlwbDhmbjIweTNuM2tvNzhmOHMyMHc4In0.5J2Vqh0EG8gAVjEWGxnNBw'
@@ -16,15 +21,19 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 var markerStart = L.marker(MAP.startLocation).addTo(mymap);
 var markerFinish = L.marker(MAP.finishLocation).addTo(mymap);
 
-navigator.geolocation.watchPosition(render);
+var marker = null;
 
-function render(pos) {
-    var lat = pos.coords.latitude;
-    var lon = pos.coords.longitude;
-    var circle = L.circle([lat, lon], {
-        color: 'blue',
-        fillColor: 'blue',
-        fillOpacity: 0.5,
-        radius: 20
-    }).addTo(mymap);
-}
+mymap.on("locationfound", function(location) {
+    if (!marker)
+        marker = L.userMarker(location.latlng, {pulsing:true, accuracy:30, smallIcon:true}).addTo(mymap);
+    
+    marker.setLatLng(location.latlng);
+    marker.setAccuracy(location.accuracy);
+});
+
+mymap.locate({
+    watch: false,
+    locate: true,
+    setView: false,
+    enableHighAccuracy: true
+});
